d98444db8e38f023dcd6662a6066b38d527bff16
Lion504 <lehtonen6677@gmail.com>
2025-08-31 23:32:55 +0300

    update

--- assignment1/src/ServiceDemo.java
+++ assignment1/src/ServiceDemo.java
@@ -4,34 +4,26 @@ public class ServiceDemo {
 
         // Create service point
         ServicePoint servicePoint = new ServicePoint();
-
-
         // Generate customers
-        System.out.println("\n1. Generating customers...");
         CustomerGenerator.setCustomer(servicePoint, 3);
-
         // Serve customers
-        System.out.println("\n2. Starting service...");
         servicePoint.serve();
-
         // Display statistics
-        System.out.println("\n3. Service statistics:");
         ServiceStatistics.calculateStatistic(servicePoint.getSerRecord());
 
         // Multiple test
         runMultipleTests();
-        System.out.println("\n" + "=".repeat(80) + "\n");
     }
 
     private static void runMultipleTests() {
-        System.out.println("\nMultiple Test Runs");
 
         int numberOfRuns = 3;
         int customersPerRun = 4;
-
+        System.out.printf("\n%s Time Tests Runs\n", numberOfRuns);
+        System.out.println("-".repeat(50));
         for (int run = 1; run <= numberOfRuns; run++) {
-            System.out.println("\nRun " + run + " time for " + customersPerRun + " customers");
-            System.out.println("-".repeat(60));
+            System.out.println("\n" + run + " time for " + customersPerRun + " customers");
+            System.out.println("-".repeat(50));
 
             ServicePoint servicePoint = new ServicePoint();
             CustomerGenerator.setCustomer(servicePoint, customersPerRun);
