6069ec0e4d4fc3d8a6bb84214418e788d564c360
Lion504 <lehtonen6677@gmail.com>
2025-08-31 23:22:26 +0300

    update

--- assignment1/src/CustomerGenerator.java
+++ assignment1/src/CustomerGenerator.java
@@ -1,21 +1,12 @@
 public class CustomerGenerator {
 
-    public static void setCustomer(ServicePoint servicePoint, int numberOfCustomers) {
-        System.out.println("Generating " + numberOfCustomers + " customers");
+    public static void setCustomer(ServicePoint serPoint, int customerNums) {
+        System.out.println("Generate " + customerNums + " customers");
 
-        for (int i = 0; i < numberOfCustomers; i++) {
+        for (int i = 0; i < customerNums; i++) {
             Customer customer = new Customer();
-            servicePoint.addToQueue(customer);
-
-            // Small delay between customer arrivals (optional)
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
+            serPoint.addToQueue(customer);
+            System.out.println("Queue size: " + serPoint.queueSize());
         }
-
-        System.out.println("Generated " + numberOfCustomers + " customers");
-        System.out.println("Queue size: " + servicePoint.queueSize());
     }
 }
